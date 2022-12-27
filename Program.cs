97433d5d2e2187e51f8e2b60723da89f5bad62aa
kycok1 <118672220+kycok1@users.noreply.github.com>
2022-12-27 14:41:04 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -142,38 +142,116 @@
 // }
 
 // task 38     Я не понимаю суть проблемы, делал как предыдущие задания
-Console.WriteLine ("Введите размер массива: ");
-int length = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите минимальное значение массива: ");
-int min = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите максимальное значение массива: ");
-int max = Convert.ToInt32(Console.ReadLine());
-int[] Array = GetArray(length, min, max);
-Console.WriteLine($"[{string.Join(", ", Array)}]");
-FindDifference( Array, max, min);
-int[] GetArray(int size, int minValue, int maxValue);
-{
-    Random rnd = new Random();
-    int[] result = new int[size];
-    for(int i = 0; i< result.Length; i ++)
-    {
-        result[i] = rnd.Next(minValue, maxValue + 1);
-    }
-    return result;
-}
-void FindDifference(int[] MyArray, int minVal, maxval)
-{
-    for(int i =0; i < MyArray.Length; i ++)
-    {
-        if (MyArray[i] < minVal)
-        {
-            minVal = MyArray[1];
-        }
-        if (MyArray[i]> maxVal)
-        {
-            maxval = MyArray[i];
-        }
-    }
-    Console.WriteLine($"Разница {maxVal - minVal}");
-}
+// Console.WriteLine ("Введите размер массива: ");
+// int length = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите минимальное значение массива: ");
+// int min = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите максимальное значение массива: ");
+// int max = Convert.ToInt32(Console.ReadLine());
+// int[] Array = GetArray(length, min, max);
+// Console.WriteLine($"[{string.Join(", ", Array)}]");
+// FindDifference( Array, max, min);
+// int[] GetArray(int size, int minValue, int maxValue);
+// {
+//     Random rnd = new Random();
+//     int[] result = new int[size];
+//     for(int i = 0; i< result.Length; i ++)
+//     {
+//         result[i] = rnd.Next(minValue, maxValue + 1);
+//     }
+//     return result;
+// }
+// void FindDifference(int[] MyArray, int minVal, maxval)
+// {
+//     for(int i =0; i < MyArray.Length; i ++)
+//     {
+//         if (MyArray[i] < minVal)
+//         {
+//             minVal = MyArray[1];
+//         }
+//         if (MyArray[i]> maxVal)
+//         {
+//             maxval = MyArray[i];
+//         }
+//     }
+//     Console.WriteLine($"Разница {maxVal - minVal}");
+// }
 
+// task 41
+// Console.Write("Введите числа через запятую: ");
+// int[] numbers = StringToNum(Console.ReadLine());
+// PrintArray(numbers);
+// int sum = 0;
+// for (int i = 0; i < numbers.Length; i++)
+// {
+//     if (numbers[i] > 0)
+//     {
+//         sum++;
+//     }
+// }
+// Console.WriteLine();
+// Console.WriteLine($"количество значений больше 0 = {sum}");
+
+
+// int[] StringToNum(string input)
+// {
+//     int count = 1;
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         if (input[i] == ',')
+//         {
+//             count++;
+//         }
+//     }
+
+//     int[] numbers = new int [count];
+//     int index = 0;
+
+//     for (int i = 0; i < input.Length; i++)
+//     {
+//         string temp = "";
+
+//         while (input [i] != ',')
+//         {
+//         if(i != input.Length - 1)
+//         {
+//             temp += input [i].ToString();
+//             i++;
+//         }
+//         else
+//         {
+//             temp += input [i].ToString();
+//             break;
+//         }
+//         }
+//         numbers[index] = Convert.ToInt32(temp);
+//         index++;
+//     }
+//     return numbers;
+// }
+
+
+// void PrintArray(int[] array)
+// {
+//     Console.Write("[ ");
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         Console.Write(array[i] + " ");
+//     }
+//     Console.Write("]");
+// }
+
+// task 43
+Console.WriteLine("введите значение b1");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k1");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите значение b2");
+double b2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число k2");
+double k2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (-b2 + b1)/(-k1 + k2);
+double y = k2 * x + b2;
+
+Console.WriteLine($"две прямые пересекутся в точке с координатами X: {x}, Y: {y}");
